9b59eca8c8390f596cbed0f6fcc4e9c128685d05
Salih Cantekin <salihcantekin@gmail.com>
2022-04-13 21:20:31 +0100

    Add project files.

---
+++ src/Projections/BlazorSozluk.Projections.VoteService/Program.cs
@@ -0,0 +1,10 @@
+using BlazorSozluk.Projections.VoteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
